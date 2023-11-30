14bd58b1dd30bb4b87e57c6854271204d85dd7cf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-30 16:53:39 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- editUser.php
+++ editUser.php
@@ -1,1 +1,15 @@
 <?php include("headers/header.php"); ?>
+
+<br><br>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            User Update
+        </div>
+        <div class="card-body">
+            <form action="" method="POST">
+                <label for="fn">User First Name : </label>
+            </form>
+        </div>
+    </div>
+</div>
