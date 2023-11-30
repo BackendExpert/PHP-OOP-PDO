48da52d1fad73ea505913484914f1b6b01184fc1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-30 17:20:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- editUser.php
+++ editUser.php
@@ -1,6 +1,6 @@
 <?php 
     include("headers/header.php"); 
-    include("includes/class.autoload.inc.php");
+    include("includes/class-autoload.inc.php");
 
     $users = new users();
     $user = $users->editUser($_GET['id']);
@@ -14,14 +14,15 @@
         </div>
         <div class="card-body">
             <a href="index.php"><button class="btn btn-primary">Back</button></a><br><br>
-            <form action="user.process.php" method="POST">
+            <form action="user.process.php?send=update" method="POST">
                 <label for="fn">User First Name : </label>
-                <input type="text" name="update-fn" id="" class="form-control"> <br>
+                <input type="text" name="update-fn" id="" class="form-control" value="<?= $user['fname']?>"> <br>
                 <label for="ln">User Last Name : </label>
-                <input type="text" name="update-ln" id="" class="form-control"><br>
+                <input type="text" name="update-ln" id="" class="form-control" value="<?= $user['lname']?>"><br>
 
                 <label for="address">User Address : </label>
-                <textarea name="update-address" id="" cols="30" rows="10" class="form-control">
+                <textarea name="update-address" id="" cols="10" rows="10" class="form-control">
+                    <?= $user['address_user']?>
                 </textarea>
                 <br>
 
