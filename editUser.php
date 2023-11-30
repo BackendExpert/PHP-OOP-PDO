e80e3d6a3cd6a22ae658fa969dbc8821436bd0ea
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-30 17:00:37 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- editUser.php
+++ editUser.php
@@ -7,8 +7,20 @@
             User Update
         </div>
         <div class="card-body">
+            <a href="index.php"><button class="btn btn-primary">Back</button></a><br><br>
             <form action="" method="POST">
                 <label for="fn">User First Name : </label>
+                <input type="text" name="update-fn" id="" class="form-control"> <br>
+                <label for="ln">User Last Name : </label>
+                <input type="text" name="update-ln" id="" class="form-control"><br>
+
+                <label for="address">User Address : </label>
+                <textarea name="update-address" id="" cols="30" rows="10" class="form-control">
+                </textarea>
+                <br>
+
+                <input type="submit" value="Update User" class="btn btn-success" name="update_user">               
+
             </form>
         </div>
     </div>
