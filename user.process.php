e8cc49e1db0b33210b2547a79337861a5eec5576
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-11-30 17:25:02 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- user.process.php
+++ user.process.php
@@ -10,3 +10,12 @@
 
         $users->addUser($fn, $ln, $address);
     }
+
+    elseif(isset($_POST['update_user'])) {
+        $id = $_GET['id'];
+        $update_fn = $_POST['update-fn'];
+        $update_ln = $_POST['update-ln'];
+        $update_address = $_POST['update-address'];
+
+        $users->updateUser($update_fn, $update_ln, $update_address, $id);
+    }
